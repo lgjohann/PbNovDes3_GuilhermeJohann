506fd2795729f71126a1fd4d76c097466a227368
Guilherme Johann <guilherme4695@gmail.com>
2025-01-22 08:40:02 -0300

    chore: criação do projeto e configuração básica do application.yml

---
+++ ms-event-manager/src/main/java/com/johann/mseventmanager/MsEventManagerApplication.java
@@ -0,0 +1,13 @@
+package com.johann.mseventmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEventManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsEventManagerApplication.class, args);
+    }
+
+}
