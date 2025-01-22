453060f2deea687103019a4a7751f842408b2865
Guilherme Johann <guilherme4695@gmail.com>
2025-01-22 09:07:31 -0300

    feat: criação da entidade Event

---
+++ ms-event-manager/src/main/java/com/johann/mseventmanager/entity/Event.java
@@ -0,0 +1,38 @@
+package com.johann.mseventmanager.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Document(collection = "events")
+@Data
+@NoArgsConstructor
+public class Event {
+
+    @Id
+    private String id;
+    private String eventName;
+    private LocalDateTime dateTime;
+    private String cep;
+    private String logradouro;
+    private String bairro;
+    private String cidade;
+    private String uf;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Event event = (Event) o;
+        return Objects.equals(id, event.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
