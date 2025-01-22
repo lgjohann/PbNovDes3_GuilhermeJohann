63a4a22666560006a0add558886b408819a0620d
Guilherme Johann <guilherme4695@gmail.com>
2025-01-22 12:08:16 -0300

    feat: adição de validação no atributo do nome do evento e data no EventCreateDto

--- ms-event-manager/src/main/java/com/johann/mseventmanager/web/dto/EventCreateDto.java
+++ ms-event-manager/src/main/java/com/johann/mseventmanager/web/dto/EventCreateDto.java
@@ -1,5 +1,6 @@
 package com.johann.mseventmanager.web.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,9 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class EventCreateDto {
 
+    @NotBlank
     private String eventName;
+    @NotBlank
     private LocalDateTime dateTime;
     @Pattern(regexp = "^[0-9]{5}-[0-9]{3}$", message = "Invalid cep format")
     private String cep;
