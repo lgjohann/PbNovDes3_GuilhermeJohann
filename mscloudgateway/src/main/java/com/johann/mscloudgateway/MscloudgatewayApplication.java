b2de90fda799f20db465d51ec08f27011b847943
Guilherme Johann <guilherme4695@gmail.com>
2025-01-22 11:44:23 -0300

    feat: criação e configuração básica do gateway e servereureka

---
+++ mscloudgateway/src/main/java/com/johann/mscloudgateway/MscloudgatewayApplication.java
@@ -0,0 +1,26 @@
+package com.johann.mscloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MscloudgatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MscloudgatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder
+                .routes()
+                .route( r -> r.path("/api/v1/events/**").uri("lb://ms-event-manager") )
+                .route( r -> r.path("/api/v1/tickets/**").uri("lb://ms-tickets-manager") )
+                .build();
+    }
+}
---
+++ servereureka/src/test/java/com/johann/servereureka/ServereurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johann.servereureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServereurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
