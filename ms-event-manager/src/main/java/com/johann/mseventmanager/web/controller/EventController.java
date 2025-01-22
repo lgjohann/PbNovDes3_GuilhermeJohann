95941f1c5e6a5270d4ec7873bb6064bf9aae0310
Guilherme Johann <guilherme4695@gmail.com>
2025-01-22 16:38:39 -0300

    chore: adicionado swagger nas dependências e feito configuração básica, documentado POST /events

--- ms-event-manager/src/main/java/com/johann/mseventmanager/web/controller/EventController.java
+++ ms-event-manager/src/main/java/com/johann/mseventmanager/web/controller/EventController.java
@@ -4,7 +4,13 @@ import com.johann.mseventmanager.entity.Event;
 import com.johann.mseventmanager.service.EventService;
 import com.johann.mseventmanager.web.dto.EventCreateDto;
 import com.johann.mseventmanager.web.dto.EventResponseDto;
+import com.johann.mseventmanager.web.exception.ErrorMessage;
 import com.johann.mseventmanager.web.mapper.EventMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,9 +23,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "api/v1/events")
+@Tag(name = "Eventos", description = "Contém os recursos para gerenciamento de eventos")
 public class EventController {
 
     private final EventService eventService;
+
+    @Operation(
+            summary = "Criar um novo produto.",
+            description = "Recurso para criar um novo produto.",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Recurso criado com sucesso",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = EventResponseDto.class))),
+                    @ApiResponse(responseCode = "404", description = "O cep informado para criação do evento não foi encontrado na base de dados",
+                            content = @Content(mediaType = " application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "422", description = "Não foi possível criar o recurso, pois os dados de entrada são inválidos",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorMessage.class)))
+            }
+    )
 
     @PostMapping
     public ResponseEntity<EventResponseDto> create(@RequestBody @Valid EventCreateDto eventCreateDto){
