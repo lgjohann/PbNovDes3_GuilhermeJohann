005e1c9bca3776253c3c35a06446f93ec2d67eb5
Guilherme Johann <guilherme4695@gmail.com>
2025-01-27 13:05:35 -0300

    feat: implementação do PUT /update-ticket/{id}

--- ms-ticket-manager/src/main/java/com/johann/msticketmanager/service/TicketService.java
+++ ms-ticket-manager/src/main/java/com/johann/msticketmanager/service/TicketService.java
@@ -6,6 +6,7 @@ import com.johann.msticketmanager.exception.EventNotFound;
 import com.johann.msticketmanager.exception.TicketNotFound;
 import com.johann.msticketmanager.repository.TicketRepository;
 import com.johann.msticketmanager.web.dto.EventDto;
+import com.johann.msticketmanager.web.dto.TicketCreateDto;
 import com.johann.msticketmanager.web.dto.TicketResponseDto;
 import com.johann.msticketmanager.web.dto.mapper.TicketMapper;
 import feign.FeignException;
@@ -50,4 +51,29 @@ public class TicketService {
         responseDto.setEvent(eventDto);
         return responseDto;
     }
+
+    @Transactional
+    public TicketResponseDto updateTicket(BigInteger id, TicketCreateDto ticketUpdate) {
+        try {
+
+            Ticket ticket = ticketRepository.findById(id).orElseThrow(() -> new TicketNotFound(String.format("Ticket with id %d not found", id)));
+
+            EventDto eventDto = EventDto.toEvent(msEventClient.findEventById(ticketUpdate.getEventId()));
+            ticket.setEventId(ticketUpdate.getEventId());
+            ticket.setEventName(ticketUpdate.getEventName());
+
+            ticket.setCustomerName(ticketUpdate.getCustomerName());
+            ticket.setCpf(ticketUpdate.getCpf());
+            ticket.setCustomerMail(ticketUpdate.getCustomerMail());
+            ticket.setBrlAmount(ticketUpdate.getBrlAmount());
+            ticket.setUsdAmount(ticketUpdate.getUsdAmount());
+            ticketRepository.save(ticket);
+
+            TicketResponseDto responseDto = TicketMapper.toDto(ticket);
+            responseDto.setEvent(eventDto);
+            return responseDto;
+        } catch (FeignException.FeignClientException.NotFound e) {
+            throw new EventNotFound("Event not found");
+        }
+   }
 }
--- ms-ticket-manager/src/main/java/com/johann/msticketmanager/web/controller/TicketController.java
+++ ms-ticket-manager/src/main/java/com/johann/msticketmanager/web/controller/TicketController.java
@@ -1,6 +1,5 @@
 package com.johann.msticketmanager.web.controller;
 
-import com.johann.msticketmanager.entity.Ticket;
 import com.johann.msticketmanager.service.TicketService;
 import com.johann.msticketmanager.web.dto.TicketCreateDto;
 import com.johann.msticketmanager.web.dto.TicketResponseDto;
@@ -63,6 +62,29 @@ public class TicketController {
     @GetMapping(value = "/get-ticket/{id}")
     public ResponseEntity<TicketResponseDto> getTicket(@PathVariable BigInteger id){
         TicketResponseDto dto = ticketService.findTicketById(id);
+        return ResponseEntity.ok(dto);
+    }
+
+    @Operation(
+            summary = "Atualizar um ingresso.",
+            description = "Recurso para atualizar informações de um ingresso.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Recurso atualizado com sucesso",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = TicketResponseDto.class))),
+                    @ApiResponse(responseCode = "404", description = "Possiveis causas desse erro: <br>"+
+                            "- O ID do ingresso informado não foi encontrado na base de dados"+
+                            "- O ID do evento informado na alteração não foi encontrado na base de dados",
+                            content = @Content(mediaType = " application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "422", description = "Não foi possível atualizar o recurso, pois os dados de entrada são inválidos",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorMessage.class)))
+            }
+    )
+    @PutMapping(value = "/update-ticket/{id}")
+    public ResponseEntity<TicketResponseDto> updateTicket(@PathVariable BigInteger id, @RequestBody @Valid TicketCreateDto ticketUpdate) {
+        TicketResponseDto dto = ticketService.updateTicket(id ,ticketUpdate);
         return ResponseEntity.ok(dto);
     }
 }
