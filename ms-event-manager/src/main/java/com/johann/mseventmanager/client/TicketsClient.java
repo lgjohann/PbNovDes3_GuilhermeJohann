8b8e2538babe0ade8ac418473255c3f80cbb44e1
Guilherme Johann <guilherme4695@gmail.com>
2025-01-30 13:31:21 -0300

    refactor: remoção do gateway e eurekaserver

--- ms-event-manager/src/main/java/com/johann/mseventmanager/client/TicketsClient.java
+++ ms-event-manager/src/main/java/com/johann/mseventmanager/client/TicketsClient.java
@@ -1,14 +1,12 @@
 package com.johann.mseventmanager.client;
 
-import com.johann.mseventmanager.web.dto.clients.TicketResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.List;
 
-@FeignClient(path = "/api/v1/tickets", name = "ms-ticket-manager")
+@FeignClient(url = "localhost:8081/api/v1/tickets", name = "ms-ticket-manager")
 public interface TicketsClient {
 
     @GetMapping(value = "/check-tickets-by-event/{eventId}")
